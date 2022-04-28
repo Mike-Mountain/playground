dcfbb2bd978ad1e10115e1107d1600ba5825e6ef
Michael <mikemountain.dev@gmail.com>
2022-04-28 15:15:25 +0200

    Update to latest NX

--- libs/container-lib/jest.config.ts
+++ libs/container-lib/jest.config.ts
@@ -1,6 +1,6 @@
 module.exports = {
-  displayName: 'shared-shared-lib',
-  preset: '../../../jest.preset.js',
+  displayName: 'container-lib',
+
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
@@ -8,7 +8,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../../coverage/libs/shared/shared-lib',
+  coverageDirectory: '../../coverage/libs/container-lib',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
@@ -18,4 +18,5 @@ module.exports = {
     'jest-preset-angular/build/serializers/ng-snapshot',
     'jest-preset-angular/build/serializers/html-comment',
   ],
+  preset: '../../jest.preset.ts',
 };
--- libs/games/code-master/jest.config.ts
+++ libs/games/code-master/jest.config.ts
@@ -1,6 +1,6 @@
 module.exports = {
-  displayName: 'games-hangman',
-  preset: '../../../jest.preset.js',
+  displayName: 'games-code-master',
+
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
@@ -8,7 +8,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../../coverage/libs/games/hangman',
+  coverageDirectory: '../../../coverage/libs/games/code-master',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
@@ -18,4 +18,5 @@ module.exports = {
     'jest-preset-angular/build/serializers/ng-snapshot',
     'jest-preset-angular/build/serializers/html-comment',
   ],
+  preset: '../../../jest.preset.ts',
 };
