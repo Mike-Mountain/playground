7d8cea34203a64bdc7aa352ea31f3809a1c2d6ee
Michael <mikemountain.dev@gmail.com>
2022-05-07 20:56:14 +0200

    Fix games routing not working

--- libs/games/games-shared/src/lib/games-games-shared.module.ts
+++ libs/games/games-shared/src/lib/games-games-shared.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GamesLandingComponent } from './components/games-landing/games-landing.component';
 import { GameMenuComponent } from './components/game-menu/game-menu.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
-  imports: [CommonModule,],
+  imports: [CommonModule, RouterModule,],
   declarations: [GamesLandingComponent, GameMenuComponent],
   exports: [GamesLandingComponent, GameMenuComponent],
 })
